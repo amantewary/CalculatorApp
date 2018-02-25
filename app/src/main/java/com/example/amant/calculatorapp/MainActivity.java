e0d9cb648773f545693fbfe206685041c6f0c580
Aman Tewary <aman.tewary@live.com>
2018-02-25 19:25:01 -0400

    Added operation function and layout for AppBar and Custom Dialog.

--- app/src/main/java/com/example/amant/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/amant/calculatorapp/MainActivity.java
@@ -6,45 +6,41 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private BigDecimal number = null;
+    private String operator = "=";
+    private EditText firstNum;
+    private EditText secondNum;
 
+    private Button btnEqualTo;
+    private Button btnDiv;
+    private Button btnMul;
+    private Button btnSub;
+    private Button btnAdd;
+
+    private Button btnZero;
+    private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
+    private Button btnFour;
+    private Button btnFive;
+    private Button btnSix;
+    private Button btnSeven;
+    private Button btnEight;
+    private Button btnNine;
+
+    private Button btnDecimal;
+    private Button btnSqrt;
+    private Button btnNegative;
+    private Button btnAllClear;
+    private Button btnClear;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//Using BigDecimal for better accuracy.
-        private BigDecimal number = null;
-        private String operator = "=";
-        private EditText firstNum;
-        private EditText secondNum;
-
-        private Button btnEqualTo;
-        private Button btnDiv;
-        private Button btnMul;
-        private Button btnSub;
-        private Button btnAdd;
-
-        private Button btnZero;
-        private Button btnOne;
-        private Button btnTwo;
-        private Button btnThree;
-        private Button btnFour;
-        private Button btnFive;
-        private Button btnSix;
-        private Button btnSeven;
-        private Button btnEight;
-        private Button btnNine;
-
-        private Button btnDecimal;
-        private Button btnSqrt;
-        private Button btnNegative;
-        private Button btnAllClear;
-        private Button btnClear;
-
-        @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
@@ -119,9 +115,105 @@ public class MainActivity extends AppCompatActivity {
             btnAdd.setOnClickListener(opListener);
             btnSqrt.setOnClickListener(opListener);
 
+            btnNegative.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String value = secondNum.getText().toString();
+                    if (value.length() == 0) {
+                        secondNum.setText("-");
+                    } else if (value.length() > 10) {
+                        return;
+                    } else {
+                        //This done to check if the converted value is a valid numeric type.
+                        try {
+                            BigDecimal decimalValue = new BigDecimal(value);
+                            BigDecimal negative = new BigDecimal(-1);
+                            decimalValue = decimalValue.multiply(negative);
+                            secondNum.setText(decimalValue.toString());
+                        } catch (NumberFormatException e) {
+                            secondNum.setText("");
+                        }
+                    }
+                }
+            });
+
+            btnSqrt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String value = secondNum.getText().toString();
+                    //This done to check if the converted value is a valid numeric type.
+                    try {
+                        Double sqrt = Double.valueOf(value);
+                        sqrt = Math.sqrt(sqrt);
+                        secondNum.setText(sqrt.toString());
+                    } catch (NumberFormatException e) {
+                        secondNum.setText("");
+                    }
+                }
+            });
+
+            btnAllClear.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    number = null;
+                    firstNum.setText("");
+                    secondNum.setText("");
+                    secondNum.setHint("0");
+                }
+            });
+
+            btnClear.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    secondNum.setText("");
+                    secondNum.setHint("0");
+                }
+            });
+        }
+
+
+    private void Operation(BigDecimal value, String operation) {
+        BigDecimal zero = new BigDecimal(0);
+        if (number == null) {
+            number = value;
+        } else {
+            if (operator.equals("=")) {
+                operator = operation;
+            }
+            switch (operator) {
+                case "=":
+                    number = value;
+                    secondNum.setText(null);
+                    break;
+                case "/":
+                    //For setting up scale:  https://stackoverflow.com/questions/33889019/bigdecimal-divide-with-a-large-number-of-decimal-places
+                    if (value.compareTo(zero) == 0) {
+                        number = null;
+                    } else {
+                        number = number.divide(value, 9, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
+                    }
+                    break;
+                case "*":
+                    number = number.multiply(value);
+                    break;
+                case "-":
+                    number = number.subtract(value);
+                    break;
+                case "+":
+                    number = number.add(value);
+                    break;
+            }
+        }
+
+        if (number == null) {
+            firstNum.setText(R.string.error);
+        } else {
+            Double result = number.doubleValue();
+            DecimalFormat format = new DecimalFormat("0.#########");
+            firstNum.setText(format.format(result).toString());
+            secondNum.setText("");
+        }
     }
-
-
 
 
 }
